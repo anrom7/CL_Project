ac0bc5f3ae3a7d9244a31b0a8b7e79498c354182
julia <savkivyuliya@gmail.com>
2015-05-10 11:43:25 +0300

    Part_3_1_karav

--- 5_33.py
+++
@@ -1,8 +0,0 @@
-import nltk
-pos=nltk.defaultdict(dict) 
-brown_tagged=nltk.corpus.brown.tagged_words()
-for ((w1,t1),(w2,t2)) in nltk.ibigrams(brown_tagged):
-    print w1+":", t1,[t2][:50]
-
-#w1,w2 - dictionaries
-#t1,t2 - tags
