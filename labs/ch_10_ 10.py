ee8415680a999cdd117cc23c43865eb5b4e14e3c
julia <savkivyuliya@gmail.com>
2015-06-11 08:56:58 +0300

    Labs all

--- labs/ch_10_ 10.py
+++
@@ -1,7 +0,0 @@
-(NP[NUM='sg', SEM=<\Q.(([x],[pen(x)]) + Q(x))>]
-(Det[NUM'sg', SEM=<\P Q.((([x],[]) + P(x)) + Q(x))>] a)
-(Nom[NUM='sg', SEM=<\x.([],[pen(x)])>]
-(N[NUM='sg', SEM=<\x.([],[dog(x)])>] pen))))) 
-  
- 
- 
